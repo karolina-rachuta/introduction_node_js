d77e2791d47a683cbc64b77424c46788967f64de
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-02-08 20:47:55 +0100

    dzien 1 zadanie 2

--- 01_Asynchroniczosc_w_NodeJS/02_Promise_API/promise_api.js
+++ 01_Asynchroniczosc_w_NodeJS/02_Promise_API/promise_api.js
@@ -1,5 +1,5 @@
 const getRandomDelay = (maxDelay) => {
-  return (Math.floor(Math.random() * maxDelay)) * 1000;
+  return Math.floor(Math.random() * maxDelay) * 1000;
 };
 
 const getAsyncNumbers = () => {
@@ -7,14 +7,34 @@ const getAsyncNumbers = () => {
   var errorDelay = getRandomDelay(10);
 
   return new Promise((resolve, reject) => {
-    setTimeout(
-      () => resolve([1, 3, 5, 6, 4, 2]), 
-      successDelay
-    );
+    setTimeout(() => resolve([1, 3, 5, 6, 4, 2]), successDelay);
 
-    setTimeout(
-      () => reject(new Error('No numbers found :(')), 
-      errorDelay
-    );
+    setTimeout(() => reject(new Error("No numbers found :(")), errorDelay);
   });
 };
+
+// for (var i = 0; i < 30; i++) {
+//   Promise.all([getAsyncNumbers(), getAsyncNumbers(), getAsyncNumbers()])
+//     .then((results) => {
+//       console.log("success", results);
+//     })
+//     .catch((error) => {
+//       console.log("error", error);
+//     });
+// }
+
+// Promise.allSettled([getAsyncNumbers(), getAsyncNumbers(), getAsyncNumbers()])
+//   .then((results) => {
+//     console.log("success", results);
+//   })
+//   .catch((error) => {
+//     console.log("error", error);
+//   });
+
+Promise.all([Promise.resolve(1), Promise.reject(new Error("Cośtam")), Promise.resolve(3)])
+    .then((results) => {
+      console.log("success", results);
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
