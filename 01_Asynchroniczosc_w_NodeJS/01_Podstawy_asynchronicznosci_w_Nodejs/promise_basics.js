f6ca4d32a424e30de9f7c7dbf1993e9d0e63dec6
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-02-13 16:21:40 +0100

    promice.race

--- 01_Asynchroniczosc_w_NodeJS/01_Podstawy_asynchronicznosci_w_Nodejs/promise_basics.js
+++ 01_Asynchroniczosc_w_NodeJS/01_Podstawy_asynchronicznosci_w_Nodejs/promise_basics.js
@@ -1,41 +1,41 @@
-// const getRandomDelay = (maxDelay) => {
-//   return (Math.floor(Math.random() * maxDelay)) * 1000;
-// };
-//
-// const getAsyncNumbers = () => {
-//   var successDelay = getRandomDelay(10);
-//   var errorDelay = getRandomDelay(10);
-//
-//   return new Promise((resolve, reject) => {
-//     setTimeout(
-//       () => resolve([1, 3, 5, 6, 4, 2]),
-//       successDelay
-//     );
-//
-//     setTimeout(
-//       () => reject(new Error('No numbers found :(')),
-//       errorDelay
-//     );
-//   });
-// };
+const getRandomDelay = (maxDelay) => {
+  return (Math.floor(Math.random() * maxDelay)) * 1000;
+};
+
+const getAsyncNumbers = () => {
+  var successDelay = getRandomDelay(10);
+  var errorDelay = getRandomDelay(10);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      () => resolve([1, 3, 5, 6, 4, 2]),
+      successDelay
+    );
+
+    setTimeout(
+      () => reject(new Error('No numbers found :(')),
+      errorDelay
+    );
+  });
+};
 
 // getAsyncNumbers()
 //     .finally(() => console.log("Proces zakończony"))
 //     .then((data) => console.log(data.sort()))
 //     .catch((err) => console.log(err.message));
 
-// async function main() {
-//   try {
-//     const numbers = await getAsyncNumbers();
-//     const sortedNumbers = numbers.sort();
-//     console.log(sortedNumbers);
-//   } catch (error) {
-//     console.log(error.message);
-//   } finally {
-//     console.log("Proces zakończony");
-//   }
-// }
+async function main() {
+  try {
+    const numbers = await getAsyncNumbers();
+    const sortedNumbers = numbers.sort();
+    console.log(sortedNumbers);
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    console.log("Proces zakończony");
+  }
+}
 
-// main();
+main();
 
 
--- 01_Asynchroniczosc_w_NodeJS/03_Promise_API_cancel/promise_cancel.js
+++ 01_Asynchroniczosc_w_NodeJS/03_Promise_API_cancel/promise_cancel.js
@@ -1,30 +1,31 @@
 const getRandomDelay = (maxDelay) => {
-  return (Math.floor(Math.random() * maxDelay)) * 1000;
+    return (Math.floor(Math.random() * maxDelay)) * 1000;
 };
 
 const getAsyncNumbers = () => {
-  var successDelay = getRandomDelay(10);
+    var successDelay = getRandomDelay(10);
 
-  return new Promise((resolve) => {
-    setTimeout(
-      () => resolve([1, 3, 5, 6, 4, 2]), 
-      successDelay
-    );
-  });
+    return new Promise((resolve) => {
+        setTimeout(
+            () => resolve([1, 3, 5, 6, 4, 2]),
+            successDelay
+        );
+    });
 };
 
+const newFunction = () => {
+    return new Promise((reject) => {
+setTimeout(() => {
+    reject(new Error("minely 3 s"))
+        }, 3000);
+    })
+}
 
-
-
-
-
-
-
-
-
-
-
-
+async function main () {
+const data = await Promise.race([getAsyncNumbers(), newFunction()])
+    console.log(data)
+}
+main();
 
 // function failAfter3sec() {
 //   return new Promise((_, reject) => {
