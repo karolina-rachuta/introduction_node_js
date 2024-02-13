eadb7f32de080ba656693be5caf12f04aa235bf8
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-02-13 16:38:38 +0100

    promise_none

--- 01_Asynchroniczosc_w_NodeJS/04_Promise_API_none/promise_none.js
+++ 01_Asynchroniczosc_w_NodeJS/04_Promise_API_none/promise_none.js
@@ -1,20 +1,44 @@
-function promiseNone() {
+// function promiseNone() {
+//
+// }
+//
+// promiseNone([
+//   Promise.reject(),
+//   Promise.reject(),
+//   // Promise.resolve
+// ])
+//   .then(() => {
+//     console.log('All failed :)')
+//   })
+//   .catch(() => {
+//     console.log('Something succeeded :(');
+//   });
 
+
+async function promiseNone(promises) {
+  return await Promise.allSettled(promises)
+      .then((results) => {
+        const hasFulfilled = results.some((result) => result.status === 'fulfilled');
+
+        if (hasFulfilled) {
+          throw new Error('Przynajmniej jedno zapytanie zakończyło się sukcesem.');
+        }
+
+        return results;
+      });
 }
 
 promiseNone([
   Promise.reject(),
   Promise.reject(),
-  // Promise.resolve
+  Promise.resolve
 ])
-  .then(() => {
-    console.log('All failed :)')
-  })
-  .catch(() => {
-    console.log('Something succeeded :(');
-  });
-
-
+    .then(() => {
+      console.log('All failed :)')
+    })
+    .catch(() => {
+      console.log('Something succeeded :(');
+    });
 
 
 
